2233420c6576617b6acf5790ea2ad2e017cf8801
Franco Alla <frannelfran@gmail.com>
2025-09-20 13:21:42 +0100

    feat: Realizar el constructor de la clase transicion

---
+++ src/transicion/transicion.cc
@@ -0,0 +1,17 @@
+#include "transicion.h"
+
+/**
+ * @brief Constructor de la clase Transicion
+ * @param id Identificador de la transición
+ * @param lecturaCadena Símbolo que se lee de la cadena de entrada
+ * @param lecturaPila Símbolo que se lee de la pila
+ * @param siguiente Puntero al estado siguiente
+ * @param apilar Cadena de símbolos a apilar en la pila
+ */
+Transicion::Transicion(const int& id, const char& lecturaCadena, const char& lecturaPila, Estado* siguiente, const string& apilar) {
+  id_ = id;
+  lecturaCadena_ = lecturaCadena;
+  lecturaPila_ = lecturaPila;
+  siguiente_ = siguiente;
+  apilar_ = apilar;
+}
