cc8c303d32a368d6f9284d5c3db85c48a4a66b44
Franco Alla <frannelfran@gmail.com>
2025-09-20 13:12:09 +0100

    feat: Incialización de la clase estado y transición

---
+++ src/estado/estado.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <set>
+#include "../transicion/transicion.h"
+
+#ifndef ESTADO_H
+#define ESTADO_H
+
+using namespace std;
+
+class Estado {
+  public:
+    // Constructor y destructor
+    Estado() = default;
+    Estado(const string& id, set<Transicion> transiciones, const bool& inicial = false) : id_(id), transiciones_(transiciones) {}
+
+  private:
+    string id_;
+    set<Transicion> transiciones_;
+    bool inicial_;
+};
+
+#endif // ESTADO_H
---
+++ src/transicion/transicion.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../estado/estado.h"
+
+#ifndef TRANSICION_H
+#define TRANSICION_H
+
+class Transicion {
+  public:
+    // Constructor y destructor
+    Transicion() = default;
+    Transicion(const int& id, const char& lecturaCadena, const char& lecturaPila, Estado* siguiente, const string& apilar);
+    ~Transicion() = default;
+
+    // Sobrecarga de operadores
+    inline bool operator<(const Transicion& other) const { return id_ < other.id_; }
+
+  private:
+    int id_;
+    char lecturaCadena_;
+    char lecturaPila_;
+    Estado* siguiente_;
+    string apilar_;
+};
+
+#endif // TRANSICION_H
